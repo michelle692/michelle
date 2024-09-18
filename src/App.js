a6c486b7a9c22e0c3511446c1e6c6e4fc694b30c
michelle <michelle_lee100@hotmail.com>
2024-09-18 17:22:54 -0400

    set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2}/>
+        <pointLight position={[10, 10, 10]} decay={0} intensity={Math.PI}/>
+        <mesh>
+          <boxGeometry args={[1.5, 1.5, 1.5]} />
+          <meshStandardMaterial color={'hotpink'} />
+        </mesh>
+      </Canvas> 
     </div>
   );
 }
