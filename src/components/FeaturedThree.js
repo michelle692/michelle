3a6b5fe70369f56b87bcb4c208254b56b37a89cc
michelle <michelle_lee100@hotmail.com>
2024-09-19 22:43:56 -0400

    bug fix #2

--- src/components/FeaturedThree.js
+++ src/components/FeaturedThree.js
@@ -4,9 +4,9 @@ function FeaturedThree() {
   return (
     <div className='f-three'>
         <img src="../images/pixiedust.png" alt='PixieDust, a 3D music visualizer' className="pixie-img" />
-        <h2><a href="https://pixiedust.vercel.app/" target="_blank" rel="noopener noreferrer">PixieDust</a></h2>
+        <h2><a href="https://pixiedust.vercel.app/" target="_blank" rel="noopener noreferrer" className='f-three-title'>PixieDust</a></h2>
         <p>For this project, I was inspired by the visuals that musicians might play during their live sets. This a 3D audio visualizer takes the frequencies from the user's mic input and utilizes those values to displace the pixels of any given image and scale it's color intensity.<br/> <br/>
-        <a href="https://github.com/michelle692/PixieDust" target="_blank" rel="noopener noreferrer">Github Repo</a></p>
+        <a href="https://github.com/michelle692/PixieDust" target="_blank" rel="noopener noreferrer" className='f-three-desc'>Github Repo</a></p>
 
       </div>
   );
